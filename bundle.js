887852879e9cd581b7385a5a7648a8364c47133a
Dan Ward <dan.steve@hotmail.co.uk>
2019-07-18 23:39:58 +0100

    algorithm changes

--- bundle.js
+++ bundle.js
@@ -1144,7 +1144,7 @@ var Convert = function() {
       {
         document.getElementById("output").innerHTML += '</br>'; 
       }
-  		var a = '<span style="background-color:#'  + MapRhymeToColour(inpu[i]) +'">' + inpu[i] + '</span> ';
+  		var a = '<span style="background-color:#'  + MapRhymeToColour(inpu[i]) +';">' + inpu[i] + '</span> ';
   		document.getElementById("output").innerHTML += a;  		
       document.getElementById("output").innerHTML += ' '; 
       count++;
@@ -1152,7 +1152,7 @@ var Convert = function() {
        
 	}
 
-  document.getElementById("output").innerHTML += ("WordCount = " + count-1);
+  document.getElementById("output").innerHTML += "</br>WordCount = " + (count);
 	/*
   addEvent(document, "change", function(e, field) {
   		convert(field);
@@ -1161,6 +1161,7 @@ var Convert = function() {
   
 };
 
+/*
 function MapToColour(val)
 {
   val = val.toLowerCase();
@@ -1170,44 +1171,53 @@ function MapToColour(val)
   //into numbers
   if(val.length == 1)
   {
-    col = fullColourHex((((val.charCodeAt(0)) * 2) % 256), (((val.charCodeAt(0)) * 2) % 256),(((val.charCodeAt(0)) * 2) % 256));
+    col = fullColourHex((((val.charCodeAt(0) - 64) * 2) % 256), (((val.charCodeAt(0)  - 64)) * 2) % 256),(((val.charCodeAt(0)  - 64)) * 2) % 256));
   }
   else if(val.length == 2)
   {
-    col = fullColourHex((((val.charCodeAt(0)) * 2) % 256), (((val.charCodeAt(1)) * 2) % 256),(((val.charCodeAt(1)) * 2) % 256));
+    col = fullColourHex((((val.charCodeAt(0) - 64) ) * 2) % 256), (((val.charCodeAt(1)  - 64)) * 2) % 256),(((val.charCodeAt(1)) * 2) % 256)  - 64));
   }
   else if(val.length == 3)
   {
-    col = fullColourHex((((val.charCodeAt(0)) * 2) % 256), (((val.charCodeAt(1)) * 2) % 256),(((val.charCodeAt(2)) * 2) % 256));
+    col = fullColourHex((((val.charCodeAt(0) - 64) ) * 2) % 256), (((val.charCodeAt(1)  - 64)) * 2) % 256),(((val.charCodeAt(2)) * 2) % 256)  - 64));
   }
   else
   {
-    col = fullColourHex((((val.charCodeAt(0)) * 2) % 256), (((val.charCodeAt(1)) * 2) % 256),(((val.charCodeAt(2)) * 2) % 256));
+    col = fullColourHex((((val.charCodeAt(0) - 64) ) * 2) % 256), (((val.charCodeAt(1)  - 64)) * 2) % 256),(((val.charCodeAt(2)) * 2) % 256)  - 64));
   }
   
   console.info(val + " " + val.length  +" " + col);
   //split by letter
   return col;
 }
+*/
 
 function MapRhymeToColour(val)
 {
   val = val.toLowerCase();
-  val = val.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+  val = val.replace(/[.,\/#!$%\^&\*;:{}=\-_`\n~()]/g,"")
+  var word = val;
   val = doubleMetaphone(val);
   var r = val[0];
   var g = val[1];
-  var b = levenshtein(val[0], val[1]);
+  
 
-  r = parseInt(WordToInt(r) % 256);
-  g = parseInt(WordToInt(g) % 256);
-  b = b * 12;
+  r = parseInt((WordToInt(r) + WordToInt(g)) * 5);
+
+  var lastone = val[0].slice(-1);
+  var lasttwo = val[1].slice(-1);
+  g = parseInt((WordToInt(lastone) + WordToInt(lasttwo)) * 5);
+
+  var b = levenshtein(val[0], val[1]); 
+  b = parseInt((WordToInt(word) / word.length) * 10);
   var col = 0;
   //into numbers
 
-  col = fullColourHex(r,g,b);
+  r += 128;
+  g += 128;
+  b += 128;
+  col = fullColourHex(r%256,g%256,b%256);
 
-  
   console.info(val + " " + val.length  +" " + col);
   console.info(r + " " + g  +" " + b);
   //split by letter
@@ -1219,9 +1229,9 @@ function WordToInt(word)
   var out = 0;
   for (var i = 0; i < word.length; i++)
   {
-    out += word.charCodeAt(i);
+    out += word.charCodeAt(i) - 77;
   }
-  return out / word.length;
+  return out/ word.length;
 }
 
 window.onload = function() {
